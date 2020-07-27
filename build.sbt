7d2728280b234940c4ff08baea1716ae74ab2d77
siddhusalvi <siddheshsalvi525@gmail.com>
2020-07-27 10:49:28 +0530

    DONE : Web Scraping on IMDB top 250 and Understood Jsoup Library

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "WebScraper"
+
+version := "0.1"
+
+scalaVersion := "2.12.10"
+
+// https://mvnrepository.com/artifact/org.jsoup/jsoup
+libraryDependencies += "org.jsoup" % "jsoup" % "1.12.1"
+
---
+++ src/main/scala/siddhu/Program.scala
@@ -0,0 +1,34 @@
+package siddhu
+import org.jsoup.Jsoup
+
+object Program {
+  def main(args: Array[String]): Unit = {
+    //Parsing the url and setting timeout of 6 seconds
+    val doc = Jsoup.connect("https://www.imdb.com/chart/top/").timeout(0).get()
+
+    val body = doc.select("tbody.lister-list")
+
+    //Printing the rows size in selected item
+    System.out.println(body.select("tr").size())
+
+    val data = body.select("tr")
+
+    data.forEach { element =>
+      //Scrapping title of the movie
+      var title = element.select("td.posterColumn img").attr("alt")
+      System.out.println(title)
+
+      //Scrapping the image
+      var img = element.select("td.posterColumn img").attr("src")
+      System.out.println(img)
+
+      //scrapping the year
+      var year = element.select("td.titleColumn span.secondaryInfo").text().replaceAll("[^\\d]", "")
+      println(year)
+
+      var rating = element.select("td.ratingColumn.imdbRating").text().trim
+      println(rating)
+
+    }
+  }
+}
