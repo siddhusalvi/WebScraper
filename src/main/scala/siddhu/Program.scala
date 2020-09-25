5a00c884b20100107dcfea9f56dcb7fb6334c0ae
siddhusalvi <siddheshsalvi525@gmail.com>
2020-09-25 16:55:13 +0530

    [Siddhesh] ADDED : Exception Handling

--- src/main/scala/siddhu/Program.scala
+++ src/main/scala/siddhu/Program.scala
@@ -3,32 +3,35 @@ import org.jsoup.Jsoup
 
 object Program {
   def main(args: Array[String]): Unit = {
-    //Parsing the url and setting timeout of 6 seconds
-    val doc = Jsoup.connect("https://www.imdb.com/chart/top/").timeout(0).get()
+    try {
+      //Parsing the url and setting timeout of 6 seconds
+      val doc = Jsoup.connect("https://www.imdb.com/chart/top/").timeout(0).get()
 
-    val body = doc.select("tbody.lister-list")
+      val body = doc.select("tbody.lister-list")
 
-    //Printing the rows size in selected item
-    System.out.println(body.select("tr").size())
+      //Printing the rows size in selected item
+      System.out.println(body.select("tr").size())
 
-    val data = body.select("tr")
+      val data = body.select("tr")
 
-    data.forEach { element =>
-      //Scrapping title of the movie
-      var title = element.select("td.posterColumn img").attr("alt")
-      System.out.println(title)
+      data.forEach { element =>
+        //Scrapping title of the movie
+        var title = element.select("td.posterColumn img").attr("alt")
+        System.out.println(title)
 
-      //Scrapping the image
-      var img = element.select("td.posterColumn img").attr("src")
-      System.out.println(img)
+        //Scrapping the image
+        var img = element.select("td.posterColumn img").attr("src")
+        System.out.println(img)
 
-      //scrapping the year
-      var year = element.select("td.titleColumn span.secondaryInfo").text().replaceAll("[^\\d]", "")
-      println(year)
+        //scrapping the year
+        var year = element.select("td.titleColumn span.secondaryInfo").text().replaceAll("[^\\d]", "")
+        println(year)
 
-      var rating = element.select("td.ratingColumn.imdbRating").text().trim
-      println(rating)
-
+        var rating = element.select("td.ratingColumn.imdbRating").text().trim
+        println(rating)
+      }
+    } catch {
+      case exception: Exception => println(exception)
     }
   }
 }
